5d13e480f883bfb7b591865508a9215a91049ca2
Robert Paszkowski <robert.paszkowski@wat.edu.pl>
2018-11-28 17:09:28 +0100

    Trzeba zmienić sposób wycinania par, lub pozostać przy pojedynczych "charach".

--- pesel/Program.cs
+++ pesel/Program.cs
@@ -10,6 +10,24 @@ namespace pesel
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Podaj");
+            String strPesel;
+            strPesel=Console.ReadLine();
+            //int intPesel = Convert.ToInt32(strPesel);
+            char[] litery = strPesel.ToArray();
+
+            for (int k = 0; k < litery.Length; k++)
+            {
+                Console.WriteLine(litery[k]);
+            }
+            String paraROK = strPesel.Remove(0, 2);
+            String paraMIESIAC = strPesel.Remove(2, 2);
+            String paraDZIEN = strPesel.Remove(4, 2);
+            String czworkaROK = strPesel.Remove(6, 4);
+
+            Console.WriteLine(paraDZIEN);   //pokazuje się pozostałość po wycięciu
+            Console.ReadKey();
+
         }
     }
 }
